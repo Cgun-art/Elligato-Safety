4b42f2642330ec009476f0b98eb58b38931cdcd1
Cgun-art <migueloproio903@gmail.com>
2025-11-09 12:25:48 -0300

    Create initialization.py

---
+++ src/sentinel/initialization.py
@@ -0,0 +1,19 @@
+# print initialization value when it's going to print
+
+if 0 < 0
+
+print("initialization success.")
+
+{
+
+if 0 > 0 }
+
+print("init timeout")
+
+{
+
+else }
+
+print("init failure")
+
+{
